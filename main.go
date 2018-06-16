703ec33ff2732b0d6d2b6907dce2c366805ee291
jamesliu <jamesliu@au1.ibm.com>
2018-06-16 20:28:00 +1000

    add imports and route handler entries for the REST API

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 // Define the current package for import/export purposes.
 package main
 
-// "fmt" includes a bunch of commands e.g. printing to STDOUT.
-import("fmt")
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// Initialise the Mux router.
+	// 	:= creates new variables using type inference.
+	r := mux.NewRouter()
+
+	// Route Handlers / Endpoints.
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	// Run the server.
+	// Wrapped in log.Fatal() so we get an error if it fails.
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
