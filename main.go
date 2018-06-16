a6dcae0793f43de4fd022e0299b61269cabc8b49
jamesliu <jamesliu@au1.ibm.com>
2018-06-16 20:12:08 +1000

    add obligatory hello world function

---
+++ main.go
@@ -0,0 +1,9 @@
+// Define the current package for import/export purposes.
+package main
+
+// "fmt" includes a bunch of commands e.g. printing to STDOUT.
+import("fmt")
+
+func main() {
+	fmt.Println("Hello World")
+}
